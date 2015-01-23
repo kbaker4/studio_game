83a660c4f4268c845ac9eabdfc8788e0b142e135
unknown <kevin.r.baker@KBAKERDT2.fmi.lcl>
2015-01-23 15:46:46 -0500

    added Game class

--- studio_game.rb
+++ studio_game.rb
@@ -29,21 +29,38 @@ class Player
 	end
 end
 
-player1 = Player.new("moe")
-player2 = Player.new("Larry", 60)
-player3 = Player.new("Curly", 125)
-player4 = Player.new("Shemp", 90)
-players = [player1, player2, player3]
-players.pop
-players.push(player4)
-puts "There are #{players.size} players in the game."
-players.each do |player|
-	player.blam
-	player.blam
-	player.w00t
-	puts player
+class Game
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(player)
+		@players.push(player)
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}."
+		@players.each do |player|
+			puts player
+		end
+		@players.each do |player|
+			player.w00t
+			player.w00t
+			player.blam
+			puts player
+		end
+	end
 end
 
-players.each do |player|
-	puts player.health
-end
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
