cfa2733b1e12125d46f9c63e008efa8312c2ab39
Kevin <Kbaker4@gmail.com>
2015-01-23 17:56:12 -0500

    Seperated classes into their own files

---
+++ game.rb
@@ -0,0 +1,27 @@
+require_relative 'player'
+
+class Game
+	attr_reader :title
+
+	def initialize(title)
+		@title = title
+		@players = []
+	end
+
+	def add_player(player)
+		@players.push(player)
+	end
+
+	def play
+		puts "There are #{@players.size} players in #{@title}."
+		@players.each do |player|
+			puts player
+		end
+		@players.each do |player|
+			player.w00t
+			player.w00t
+			player.blam
+			puts player
+		end
+	end
+end
---
+++ player.rb
@@ -0,0 +1,32 @@
+require_relative 'game'
+
+class Player
+	attr_reader :health, :name
+
+	def initialize(name, health = 100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def name=(new_name)
+		@name = new_name.capitalize
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
+	end
+
+	def blam
+		@health -= 10
+		puts "#{@name} got blammed!"
+	end
+
+	def w00t
+		@health += 15
+		puts "#{@name} got w00ted!"
+	end
+
+	def score
+		@health + @name.length
+	end
+end
